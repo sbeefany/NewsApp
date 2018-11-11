26c87af41deafbcd28d18558974519fc8b9d805f
Sasha <cu-009@mail.ru>
2018-11-11 13:10:34 +0300

    fixed one more comment

--- app/src/main/java/com/example/user/mycardapp/Data/NewsItem.java
+++ app/src/main/java/com/example/user/mycardapp/Data/NewsItem.java
@@ -1,13 +1,14 @@
 package com.example.user.mycardapp.Data;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
 import com.example.user.mycardapp.Data.Network.DTOModels.Multimedia;
 import com.example.user.mycardapp.Data.Network.DTOModels.Result;
 
 import java.util.Date;
 import java.util.Random;
 
-import io.reactivex.annotations.NonNull;
-import io.reactivex.annotations.Nullable;
 
 public class NewsItem {
 
@@ -53,30 +54,78 @@ public class NewsItem {
         return new Category(random.nextInt() , dtoNewsModel.getSubsection());
     }
 
+    @NonNull
     public String getUrl () {
         return url;
     }
 
+    @Nullable
     public String getTitle () {
         return title;
     }
 
+    @Nullable
     public String getImageUrl () {
         return imageUrl;
     }
 
+    @Nullable
     public Category getCategory () {
         return category;
     }
 
+    @Nullable
     public String getPublishDate () {
         return publishDate;
     }
 
+    @Nullable
     public String getPreviewText () {
         return previewText;
     }
 
+    @NonNull
+    @Override
+    public String toString () {
+        return "Url:" + getUrl()
+                + " Title:" + getTitle()
+                + " PreviewText:" + getPreviewText()
+                + " PublishDate:" + getPublishDate()
+                + " Category:" + getCategory().getName()
+                + " ImageUrl:" + getUrl();
+    }
 
+    @Override
+    public boolean equals (@Nullable Object obj) {
+        if (obj == this)
+            return true;
+        if (!(obj instanceof NewsItem))
+            return false;
+        NewsItem newsItem = ( NewsItem ) obj;
+        return newsItem.getUrl().equals(getUrl()) &&
+                newsItem.getCategory().equals(getCategory()) &&
+                newsItem.getPublishDate().equals(getPublishDate()) &&
+                newsItem.getPreviewText().equals(getPreviewText()) &&
+                newsItem.getTitle().equals(getTitle()) &&
+                newsItem.getImageUrl().equals(getImageUrl());
+
+    }
+
+    @Override
+    public int hashCode () {
+        int result = 17;
+        if (getImageUrl() != null)
+            result = 31 * result + getImageUrl().hashCode();
+        if (getTitle() != null)
+            result = 31 * result + getTitle().hashCode();
+        if (getPreviewText() != null)
+            result = 31 * result + getPreviewText().hashCode();
+        if (getPublishDate() != null)
+            result = 31 * result + getPublishDate().hashCode();
+        if (getCategory() != null)
+            result = 31 * result + getCategory().hashCode();
+        result = 31 * result + getUrl().hashCode();
+        return result;
+    }
 }
 
